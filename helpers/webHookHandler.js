2a14c0e2b069489a755492b6d74022362bd33204
saaya-code <boualimohamed74@gmail.com>
2023-12-23 14:32:07 +0100

    added presence

--- helpers/webHookHandler.js
+++ helpers/webHookHandler.js
@@ -2,14 +2,13 @@ require("dotenv").config();
 
 const webHookers = {
     "TECH": {webhookUrl:process.env.TECH_webhook, roleId:"830900803254222938"},
-    "TM":  {webhookUrl:process.env.TM_webhook, roleId:"792837124315152405"},
-    "MKT": {webhookUrl: process.env.MKT_webhook, roleId:"791432358607585291"},
+    "TM":   {webhookUrl:process.env.TM_webhook, roleId:"792837124315152405"},
+    "MKT":  {webhookUrl: process.env.MKT_webhook, roleId:"791432358607585291"},
     "EER":  {webhookUrl:process.env.EER_webhook, roleId:"792837038570471426"}
 }
 
 module.exports = function({activity_type, date, details, concerned_committes}){
   const newDetails = details || "No details were provided."
- 
       for(com of concerned_committes){
         const embed = {
           "content": `Hello <@&${webHookers[com].roleId}> a new ${activity_type} has been added.`,
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const path = require('path');
 const discord = require('discord.js');
 const client = new discord.Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers,GatewayIntentBits.MessageContent, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites]
-
 });
 // registering commands
 client.commands = new Map();
@@ -32,6 +31,8 @@ for (const file of commandFiles) {
 client.on('ready', () => {
     connectDB(process.env.MONGO_URI);
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('GDSC activities', { type: 'WATCHING' });
+    
 });
 
 
@@ -51,6 +52,8 @@ const announce = require('./helpers/announce');
 client.on(Events.MessageCreate, async message => {
     await announce(message, client);
 });
+
+
 
 
         
